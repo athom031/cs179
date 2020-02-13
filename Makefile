f7767b02918ca1407d64d97d17262ca366f0a5cd
Daniel Tan <danieltan1517@protonmail.com>
2020-02-13 11:24:36 -0800

    started phase 3

--- Makefile
+++ Makefile
@@ -1,19 +1,18 @@
-FILE=LinearSearch
+FILE=1-Basic
 JAVA=$(FILE).java
 VAPOR=$(FILE).vapor
 FOLDER=phase2-tests
 OUTPUT_FOLDER=out
+
+vaporm:
+	javac V2VM.java
+	java V2VM < Phase3Tests/$(FILE).vapor > $(FILE).vaporm
 
 run:
 	javac J2V.java
 	java J2V < $(FOLDER)/$(JAVA) > $(OUTPUT_FOLDER)/$(VAPOR)
 	cat $(OUTPUT_FOLDER)/$(VAPOR)
 	java -jar vapor.jar run $(OUTPUT_FOLDER)/$(VAPOR)
-
-vaporm:
-	javac V2VM.java
-	java V2VM < $(FILE).vapor > $(FILE).vaporm
-	java -jar vapor.jar run -mips $(FILE).vaporm
 
 
 debug:
---
+++ V2VM.java
@@ -0,0 +1,55 @@
+import cs132.util.CommandLineLauncher;
+import cs132.util.CommandLineLauncher.Exit;
+import cs132.util.ProblemException;
+import cs132.util.VFunction;
+import cs132.vapor.parser.VaporParser;
+import cs132.vapor.ast.VaporProgram;
+import cs132.vapor.ast.VBuiltIn.Op;
+import java.io.*;
+
+class V2VM extends CommandLineLauncher.TextOutput {
+
+
+  public static void main(String[] args) {
+    CommandLineLauncher.run(new V2VM(), args);
+
+  }
+
+  public void run(PrintWriter out, InputStream in, PrintWriter err, String [] args) throws Exit {
+    try {
+      VaporProgram p = V2VM.parseVapor(in, System.err);
+      for(VFunction f : p.functions) {
+
+      }
+    } catch(IOException e) {
+      System.err.println(e);
+    }
+  }
+
+  public static VaporProgram parseVapor(InputStream in, PrintStream err) throws IOException {
+    Op [] ops = {Op.Add, Op.Sub, Op.MulS, Op.Eq, Op.Lt, Op.LtS, Op.PrintIntS, Op.HeapAllocZ, Op.Error};
+    boolean allowLocals = true;
+    String [] registers = null;
+    boolean allowStack = false;
+
+    try {
+      return VaporParser.run(new InputStreamReader(in), 
+                             1, 
+                             1,
+                             java.util.Arrays.asList(ops), 
+                             allowLocals, 
+                             registers, 
+                             allowStack);
+
+    } catch(ProblemException ex) {
+      err.println(ex.getMessage());
+      return null;
+    }
+  }
+
+
+}
+
+
+
+
